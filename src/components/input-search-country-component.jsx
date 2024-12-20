57d8988e785dfb6fdcb3f24b0ecce0128803c879
fzialam <alamfauzi000@gmail.com>
2024-12-20 17:47:32 +0700

    style ReactSearchAutocomplete

--- src/components/input-search-country-component.jsx
+++ src/components/input-search-country-component.jsx
@@ -10,7 +10,7 @@ export const InputSearchCountryComponent = ({ data, onSelect, onClear }) => {
       resultStringKeyName="name"
       placeholder={"Search Country"}
       className="form-control search-container"
-      style={{ backgroundColor: "#333", color: "#fff" }}
+      styling={{ backgroundColor: "#eee", color: "#000", boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)"}}
     />
   );
 };
